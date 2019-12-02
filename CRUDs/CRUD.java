f50a602f873ab31c4cd15c8ccf9ec85182b123b8
nierema_sd2081@CEACCS.NET <nierema_sd2081@CEACCS.NET>
2019-12-02 10:28:36 +0800

    initial

--- CRUDs/CRUD.java
+++ CRUDs/CRUD.java
@@ -16,7 +16,7 @@ import java.sql.Statement;
  */
 public class CRUD {
     private ResultSet conn;
-    public boolean addData() {
+    public boolean addData(String sqlString) {
         try {
             // create a mysql database connection
             String myDriver = "com.mysql.jdbc.Driver";
@@ -25,11 +25,9 @@ public class CRUD {
             Connection conn = DriverManager.getConnection(myUrl, "root", "");
 
             Statement st = conn.createStatement();
-            
-            String query = " insert into userlist (firstname, lastname, middlename, email, password, repassword)" + "values ('?,?,?,?,?,?')"; 
 
             // note that i'm leaving "date_created" out of this insert statement
-            st.executeUpdate(query);
+            st.executeUpdate(sqlString);
             conn.close();
             return true;
         } catch (Exception e) {
@@ -40,7 +38,7 @@ public class CRUD {
 
     }
 
-    public ResultSet getData() {
+    public ResultSet getData(String sqlQuery) {
         try {
             String myDriver = "com.mysql.jdbc.Driver";
             String myUrl = "jdbc:mysql://localhost/niere";
@@ -48,9 +46,8 @@ public class CRUD {
             Connection conn = DriverManager.getConnection(myUrl, "root", "");
             
             Statement st = conn.createStatement();
-            String getData = "select * from userlist";
 
-            ResultSet rs = st.executeQuery(getData);
+            ResultSet rs = st.executeQuery(sqlQuery);
             while(rs.next()){
                 System.out.println(rs.getString("firstname") + rs.getString("lastname")+rs.getString("middlename")
                 +rs.getString("email") + rs.getString("password")+ rs.getString("repassword"));
